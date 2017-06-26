d227aa416c0bd0ed3f36397dd3624888f81509e5
Neel Alex <neelalex@Neels-MacBook-Air.local>
2017-06-26 17:45:57 +0530

    Add existing file

--- a69.cpp
+++ a69.cpp
@@ -1,3 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+int main(){
+	int n,valx,valy,valz;
+	cin>>n;
+	int x=0,y=0,z=0;
+	while(n--){
+		cin>>valx>>valy>>valz;
+		x+=valx;
+		y+=valy;
+		z+=valz;
+	}
+	if(x==0 && y==0 && z==0)
+		cout<<"YES\n";
+	else
+		cout<<"NO\n";
+	
+}
